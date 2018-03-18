c6cc9c156fb97747a83f44af87fb70ec006cd06e
kalinovcic <lovro.kalinovcic@gmail.com>
2018-03-18 13:01:24 +0100

    Licence stuff

--- lk_platform.h
+++ lk_platform.h
@@ -1,3 +1,24 @@
+//  lk_platform.h - public domain platform abstraction layer
+//  no warranty is offered or implied
+
+/*********************************************************************************************
+
+QUICK NOTES
+    @Incomplete
+
+LICENSE
+    This software is in the public domain. Anyone can use it, modify it,
+    roll'n'smoke hardcopies of the source code, sell it to the terrorists, etc.
+    No warranty is offered or implied; use this code at your own risk!
+
+    See end of file for license information.
+
+DOCUMENTATION
+    @Incomplete
+
+ *********************************************************************************************/
+
+
 #ifndef LK_PLATFORM_HEADER
 #define LK_PLATFORM_HEADER
 
@@ -768,3 +789,30 @@ int main(int argc, char** argv)
 
 #endif
 #endif
+
+
+/*********************************************************************************************
+
+THE UNLICENCE (http://unlicense.org)
+
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or distribute this 
+    software, either in source code form or as a compiled binary, for any purpose, 
+    commercial or non-commercial, and by any means.
+
+    In jurisdictions that recognize copyright laws, the author or authors of this 
+    software dedicate any and all copyright interest in the software to the public 
+    domain. We make this dedication for the benefit of the public at large and to 
+    the detriment of our heirs and successors. We intend this dedication to be an 
+    overt act of relinquishment in perpetuity of all present and future rights to 
+    this software under copyright law.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+    AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+    ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *********************************************************************************************/
