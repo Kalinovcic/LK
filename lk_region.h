e2822698729e59d7a7e2f7006656739bec0f762c
kalinovcic <lovro.kalinovcic@gmail.com>
2018-07-15 15:54:51 +0200

    lk_region update, added stack behavior

--- lk_region.h
+++ lk_region.h
@@ -1,5 +1,5 @@
-//  lk_platform.h - public domain platform abstraction layer with live code editing support
-//  no warranty is offered or implied
+/*  lk_region.h - public domain stack-based allocator */
+/*  no warranty is offered or implied */
 
 /*********************************************************************************************
 
@@ -33,29 +33,84 @@ extern "C"
 {
 #endif
 
-typedef struct LK_Region_Structure
+/* This entire preprocessor mess is here just because
+   C and C++ don't really provide a simple way to set alignment,
+   or inquire about alignment. We need alignment info to allocate,
+   and we would also like LK_Region to be cache-aligned. */
+
+#define LK__REGION_CACHE_SIZE 32
+#if defined(__cplusplus) && (__cplusplus>=201103L)
+/* Do it with C++11 features, if available. */
+#define LK__REGION_CACHE_ALIGN alignas(LK__REGION_CACHE_SIZE)
+#define LK__REGION_CACHE_ALIGN_POST
+#define LK__REGION_ALIGNOF(type) alignof(type)
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__>=201112L)
+/* Do it with C11 features, if available. */
+#define LK__REGION_CACHE_ALIGN _Alignas(LK__REGION_CACHE_SIZE)
+#define LK__REGION_CACHE_ALIGN_POST
+#define LK__REGION_ALIGNOF(type) _Alignof(type)
+#elif defined(_MSC_VER)
+/* Do it with MSVC features, if available. */
+#define LK__REGION_CACHE_ALIGN __declspec(align(LK__REGION_CACHE_SIZE))
+#define LK__REGION_CACHE_ALIGN_POST
+#define LK__REGION_ALIGNOF(type) __alignof(type)
+#elif defined(__GNUC__) || defined(__GNUG__)
+/* Do it with GCC features, if available. */
+#define LK__REGION_CACHE_ALIGN
+#define LK__REGION_CACHE_ALIGN_POST __attribute__((aligned(LK__REGION_CACHE_SIZE)))
+#define LK__REGION_ALIGNOF(type) __alignof__(type)
+#else
+/* Oh well, best of luck to you! */
+#define LK__REGION_CACHE_ALIGN
+#define LK__REGION_CACHE_ALIGN_POST
+#define LK__REGION_ALIGNOF(type) (sizeof(type) > 4 ? 8 : (sizeof(type) > 2 ? 4 : (sizeof(type) == 2 ? 2 : 1)))
+#endif
+
+/* LK_Region struct.
+   You shouldn't need to care about the members of this struct,
+   it is only in the header so that you can allocate it. */
+typedef LK__REGION_CACHE_ALIGN struct
 {
-    void* alloc_head;
+    uintptr_t page_size;
     void* page_end;
     void* cursor;
-    uintptr_t page_size;
-} LK_Region;
+    void* alloc_head;
+} LK__REGION_CACHE_ALIGN_POST LK_Region;
+
+/* Use this macro to initialize region variables. Like this:
+       LK_Region region = LK_RegionInit;
+   If you're using C++, you can also do:
+       LK_Region region = { 0 };
+       LK_Region region = {}; // C++11 */
+#define LK_RegionInit { 0, 0, 0, 0 }
 
 void* lk_region_alloc(LK_Region* region, size_t size, size_t alignment);
 void lk_region_free(LK_Region* region);
 
-#define LK__AlignmentGuess(type) (sizeof(type) > 4 ? 8 : (sizeof(type) > 2 ? 4 : (sizeof(type) == 2 ? 2 : 1)))
-
-#define LK_RegionValue(region_ptr, type)                          ((type*) lk_region_alloc((region_ptr), sizeof(type),           LK__AlignmentGuess(type)))
-#define LK_RegionArray(region_ptr, type, count)                   ((type*) lk_region_alloc((region_ptr), sizeof(type) * (count), LK__AlignmentGuess(type)))
+/* Helper macros. */
+#define LK_RegionValue(region_ptr, type)                          ((type*) lk_region_alloc((region_ptr), sizeof(type),           LK__REGION_ALIGNOF(type)))
+#define LK_RegionArray(region_ptr, type, count)                   ((type*) lk_region_alloc((region_ptr), sizeof(type) * (count), LK__REGION_ALIGNOF(type)))
 #define LK_RegionValueAligned(region_ptr, type, alignment)        ((type*) lk_region_alloc((region_ptr), sizeof(type),           (alignment)))
 #define LK_RegionArrayAligned(region_ptr, type, count, alignment) ((type*) lk_region_alloc((region_ptr), sizeof(type) * (count), (alignment)))
+
+/* LK_Region_Cursor struct.
+   You shouldn't need to care about the members of this struct,
+   it is only in the header so that you can allocate it. */
+typedef struct
+{
+    void* page_end;
+    void* cursor;
+    void* alloc_head;
+} LK_Region_Cursor;
+
+void lk_region_cursor(LK_Region* region, LK_Region_Cursor* cursor);
+void lk_region_rewind(LK_Region* region, LK_Region_Cursor* cursor);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // LK_REGION_HEADER
+#endif /* LK_REGION_HEADER */
 
 /*********************************************************************************************
 
@@ -68,32 +123,63 @@ void lk_region_free(LK_Region* region);
 #define LK_REGION_IMPLEMENTED
 
 #ifdef _WIN32
+/*********************************************************************************************
+  Windows-specific
+ *********************************************************************************************/
+#ifndef LK_REGION_DEFAULT_PAGE_SIZE
+#define LK_REGION_DEFAULT_PAGE_SIZE 0x10000 /* 64 kB */
+#endif
 
 #include <WinBase.h>
 
+void* lk_region_os_alloc(size_t size)
+{
+    return VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+}
+
+void lk_region_os_free(void* memory)
+{
+    VirtualFree(memory, 0, MEM_RELEASE);
+}
+
+#else
+#error Unrecognized operating system
+#endif
+
+/*********************************************************************************************
+  Cross-platform
+ *********************************************************************************************/
+
 void* lk_region_alloc(LK_Region* region, size_t size, size_t alignment)
 {
+    typedef uint8_t byte;
     typedef uintptr_t umm;
 
+    /* set default page size */
     umm page_size = region->page_size;
     if (!page_size)
     {
-        page_size = 0x10000; // 64 kB
+        page_size = LK_REGION_DEFAULT_PAGE_SIZE;
         region->page_size = page_size;
     }
 
-    // big allocation check
-    umm minimum_alignment = (alignment > sizeof(void*)) ? alignment : sizeof(void*);
-    umm big_allocation_threshold = page_size - minimum_alignment;
+    /* check if this is a big allocation */
+    umm big_allocation_threshold = (page_size >> 2);
     if (size > big_allocation_threshold)
     {
-        char* page = (char*) VirtualAlloc(0, size + minimum_alignment, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-        *(void**) page = region->alloc_head;
-        region->alloc_head = page;
-        return page + minimum_alignment;
+        if (alignment < sizeof(void*))
+            alignment = sizeof(void*);
+
+        byte* page = (byte*) lk_region_os_alloc(size + alignment);
+
+        void** header = (void**) page;
+        *header = region->alloc_head;
+        region->alloc_head = header;
+
+        return page + alignment;
     }
 
-    // align cursor
+    /* align cursor */
     umm cursor_address = (umm) region->cursor;
     umm remainder = cursor_address & (umm)(alignment - 1);
     if (remainder)
@@ -101,50 +187,82 @@ void* lk_region_alloc(LK_Region* region, size_t size, size_t alignment)
         cursor_address += alignment - remainder;
     }
 
-    // resize check
+    /* end of page check */
     umm end_address = cursor_address + size;
     if (end_address > (umm) region->page_end)
     {
-        char* page = (char*) VirtualAlloc(0, page_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-        char* page_end = page + page_size;
+        /* allocate another page */
+        byte* page = (byte*) lk_region_os_alloc(page_size);
+        byte* page_end = page + page_size;
 
-        void** cursor = (void**) page;
-        *(cursor++) = region->alloc_head;
+        void** header = (void**) page;
+        *header = region->alloc_head;
 
-        region->alloc_head = page;
         region->page_end = page_end;
-        region->cursor = cursor;
+        region->alloc_head = header;
 
-        return lk_region_alloc(region, size, alignment);
+        void* cursor = header + 1;
+
+        /* realign */
+        cursor_address = (umm) cursor;
+        remainder = cursor_address & (umm)(alignment - 1);
+        if (remainder)
+        {
+            cursor_address += alignment - remainder;
+        }
+
+        end_address = cursor_address + size;
     }
 
-    // success
-    void* cursor = (void*) cursor_address;
+    /* success */
+    void* result = (void*) cursor_address;
     region->cursor = (void*) end_address;
-    return cursor;
+    return result;
 }
 
 void lk_region_free(LK_Region* region)
 {
-    void* page = region->alloc_head;
-    while (page)
+    void* memory = region->alloc_head;
+    while (memory)
     {
-        void* next_page = *(void**) page;
-        VirtualFree(page, 0, MEM_RELEASE);
-        page = next_page;
+        void** header = (void**) memory;
+        void* next_memory = *header;
+
+        lk_region_os_free(memory);
+        memory = next_memory;
     }
 
-    region->alloc_head = NULL;
-    region->page_end = NULL;
-    region->cursor = NULL;
+    region->page_end = 0;
+    region->cursor = 0;
+    region->alloc_head = 0;
 }
 
-#else // !defined(_WIN32)
-#error Unrecognized operating system
-#endif
+void lk_region_cursor(LK_Region* region, LK_Region_Cursor* cursor)
+{
+    cursor->page_end   = region->page_end;
+    cursor->cursor     = region->cursor;
+    cursor->alloc_head = region->alloc_head;
+}
 
-#endif // LK_REGION_IMPLEMENTED
-#endif // LK_REGION_IMPLEMENTATION
+void lk_region_rewind(LK_Region* region, LK_Region_Cursor* cursor)
+{
+    void* memory = region->alloc_head;
+    while (memory != cursor->alloc_head)
+    {
+        void** header = (void**) memory;
+        void* next_memory = *header;
+
+        lk_region_os_free(memory);
+        memory = next_memory;
+    }
+
+    region->page_end   = cursor->page_end;
+    region->cursor     = cursor->cursor;
+    region->alloc_head = cursor->alloc_head;
+}
+
+#endif /* LK_REGION_IMPLEMENTED */
+#endif /* LK_REGION_IMPLEMENTATION */
 
 
 /*********************************************************************************************
